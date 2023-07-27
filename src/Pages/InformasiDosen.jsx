52f4618d4e2ce0934450cb5134bf3fbe25883984
mancode <mancode700@gmail.com>
2023-07-27 13:54:56 +0700

    add: information dosen and component information dosen

--- src/Pages/InformasiDosen.jsx
+++ src/Pages/InformasiDosen.jsx
@@ -1,18 +1,25 @@
-import React, { useContext, useEffect } from 'react'
-import Layout from '../Components/Layout'
-import { GlobalState } from '../Context/Context'
+import { useContext, useEffect } from "react";
+import Layout from "../Components/Layout";
+import { GlobalState } from "../Context/Context";
+import CardProfile from "../Components/InformasiDosen/CardProfile";
 
 const InformasiDosen = () => {
-  const {dispatch} = useContext(GlobalState)
-  
+  const { dispatch } = useContext(GlobalState);
+
   useEffect(() => {
-    dispatch({type: "UPDATE_PAGE", payload: { page: "Informasi", subPage: "Informasi Dosen" }})
-  }, [])
+    dispatch({ type: "UPDATE_PAGE", payload: "Informasi Dosen" });
+  }, []);
   return (
     <Layout>
-      <div>InformasiDosen</div>
-    </Layout>
-  )
-}
+      <section className="px-20 pb-10 flex flex-col gap-5">
+        <h1 className="text-[30px] px-10 py-10 pb-5 font-bold ">
+          Informasi Dosen
+        </h1>
 
-export default InformasiDosen
+        <CardProfile />
+      </section>
+    </Layout>
+  );
+};
+
+export default InformasiDosen;
