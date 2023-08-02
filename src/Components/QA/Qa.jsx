6acd5395e2eb02d6cc4445042c48a9ae1afd0167
mancode <mancode700@gmail.com>
2023-08-02 11:04:11 +0700

    Add: QA and Timer

---
+++ src/Components/QA/Qa.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { BsWhatsapp } from "react-icons/bs";
+
+const Qa = () => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
+
+  return (
+    <a
+      href="https://api.whatsapp.com/send?phone=xxxxxxxxxxxx"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed right-10 bottom-10 bg-green-500 p-5 rounded-full text-white shadow-lg hover:bg-green-600 transition-all duration-300"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {isHovering ? (
+        <span className="text-2xl">Q/A</span>
+      ) : (
+        <span>
+          <BsWhatsapp className="text-4xl" />
+        </span>
+      )}
+    </a>
+  );
+};
+
+export default Qa;
