1db95852a6c321495c3aae0dd6d5cc0f8d2efc28
mancode <mancode700@gmail.com>
2023-09-09 15:02:00 +0700

    Update: Tag Html Nav, Home to ChAkra UI

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,6 +13,28 @@ import edlinkLink from "../../Assets/edlink-link.png";
 import siakadLink from "../../Assets/siakad-link.png";
 import sisterLink from "../../Assets/sister-link.png";
 import ojBrand from "../../Assets/ojs_brand.png";
+import {
+  Box,
+  Button,
+  Flex,
+  IconButton,
+  Link,
+  Spinner,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  useColorMode,
+  useDisclosure,
+  Text,
+  Heading,
+  Image,
+  Card,
+} from "@chakra-ui/react";
 
 const visi = [
   {
@@ -58,37 +80,37 @@ const mission = [
 const refrention = [
   {
     name: "Elita",
-    img: elitaLink,
+    Image: elitaLink,
     desc: "Elita",
     link: "http://kuliah.politama.ac.id/",
   },
   {
     name: "EdLink",
-    img: edlinkLink,
+    Image: edlinkLink,
     desc: "EdLink",
     link: "https://edlink.id/",
   },
   {
     name: "Siakad",
-    img: siakadLink,
+    Image: siakadLink,
     desc: "Siakad",
     link: "https://siakad.politama.ac.id/index.php/login",
   },
   {
     name: "Sister",
-    img: sisterLink,
+    Image: sisterLink,
     desc: "Sister",
     link: "http://sister.politama.ac.id/dashboard/",
   },
   {
     name: "Jurnal",
-    img: ojBrand,
+    Image: ojBrand,
     desc: "Jurnal",
     link: "http://jurnal.politama.ac.id/index.php/politeknosains",
   },
   {
     name: "PMB",
-    img: logoPolitama,
+    Image: logoPolitama,
     desc: "PMB",
     link: "https://pmb.politama.ac.id/index.php/pendaftaran_pmb",
   },
@@ -96,6 +118,7 @@ const refrention = [
 
 const Home = () => {
   const { dispatch } = useContext(GlobalState);
+  const { colorMode, toggleColorMode } = useColorMode();
 
   useEffect(() => {
     dispatch({
@@ -107,7 +130,7 @@ const Home = () => {
   return (
     <Layout>
       <>
-        <section className="relative w-full lg:h-[400px] md:pt-0">
+        <Flex className="relative w-full lg:h-[400px] md:pt-0">
           <Swiper
             centeredSlides={true}
             autoplay={{
@@ -122,155 +145,220 @@ const Home = () => {
             className="mySwiper w-full h-full"
           >
             <SwiperSlide>
-              <img
+              <Image
                 className="w-full h-full object-cover"
                 src={banner1}
                 alt=""
               />
             </SwiperSlide>
             <SwiperSlide>
-              <img
+              <Image
                 className="w-full h-full object-cover"
                 src={banner2}
                 alt=""
               />
             </SwiperSlide>
             <SwiperSlide>
-              <img
+              <Image
                 className="w-full h-full object-cover"
                 src={banner3}
                 alt=""
               />
             </SwiperSlide>
           </Swiper>
-          <div className="absolute top-0 z-10 w-full h-full flex flex-col justify-center items-center backdrop-brightness-50">
-            <p className="text-center text-gray-200 md:text-[20px] lg:text-[29px] font-bold">
+          <Box className="absolute top-0 z-10 w-full h-full flex flex-col justify-center items-center backdrop-brightness-50">
+            <Text className="text-center text-gray-200 md:text-[20px] lg:text-[29px] font-bold">
               Selamat Datang di Laman
-            </p>
-            <h1 className="text-center text-blue-500 text-[20px] sm:text-[38px] md:text-[48px] lg:text-[59px] text-xl font-bold sm:py-5">
+            </Text>
+            <Heading
+              as="h2"
+              className="text-center text-blue-500 text-[20px] sm:text-[38px] md:text-[48px] lg:text-[59px] text-xl font-bold sm:py-5"
+            >
               MANAJMEN INFORMATIKA
-            </h1>
-            <p className="text-center text-gray-200 md:text-[20px] lg:text-[29px] font-bold">
+            </Heading>
+            <Text className="text-center text-gray-200 md:text-[20px] lg:text-[29px] font-bold">
               Politeknik Pratama Mulia Surakarta
-            </p>
-          </div>
-        </section>
-        <section className="w-full h-fit md:px-20 px-10 py-10 flex md:flex-row flex-col-reverse gap-10 justify-between item-center">
-          <div className="md:px-10 md:border-r md:border-gray-500">
-            <div className="md:w-fit w-full mb-3">
-              <h1 className="text-3xl font-semibold text-gray-700 border-b-2 border-red-500">
+            </Text>
+          </Box>
+        </Flex>
+        <Box
+          as="section"
+          className={`w-full h-fit md:px-20 px-10 py-10 flex md:flex-row flex-col-reverse gap-10 justify-between item-center ${
+            colorMode === "dark" ? "bg-brandTabs-900" : "bg-blue-100"
+          }`}
+        >
+          <Box className="md:px-10 md:border-r md:border-gray-500">
+            <Box className="md:w-fit w-full mb-3">
+              <Heading
+                as="h1"
+                className={`text-3xl font-semibold border-b-2 ${
+                  colorMode === "dark" ? "border-yellow-600" : "border-red-600"
+                }`}
+              >
                 TAUTAN
-              </h1>
-            </div>
-            <div className="w-full h-fit flex md:flex-col flex-wrap justify-center gap-2">
+              </Heading>
+            </Box>
+            <Box className="w-full h-fit flex md:flex-col flex-wrap justify-center gap-2">
               {refrention.map((ref, i) => {
                 return (
-                  <div key={i} className="felx flex-col gap-3">
-                    <div className="relative mb-3 group md:w-[120px] w-[80px] h-fit aspect-square overflow-hidden">
-                      <img
-                        src={ref.img}
+                  <Box key={i} className="felx flex-col gap-3">
+                    <Box className="relative mb-3 group md:w-[120px] w-[80px] h-fit aspect-square overflow-hidden">
+                      <Image
+                        src={ref.Image}
                         className="w-full h-full"
                         alt="elita-link"
                       />
-                      <div className="absolute opacity-0 group-hover:top-0 hover:opacity-100 duration-300 w-full h-full backdrop-brightness-75 flex justify-center items-center cursor-pointer">
-                        <a
+                      <Box className="absolute opacity-0 group-hover:top-0 hover:opacity-100 duration-300 w-full h-full backdrop-brightness-75 flex justify-center items-center cursor-pointer">
+                        <Link
                           rel="noopener noreferrer"
                           href={ref.link}
                           target="_blank"
-                          className="bg-green-300 px-2 py-2 rounded w-[80px] h-[80px] flex flex-col justify-center items-center"
+                          className={`${
+                            colorMode === "dark"
+                              ? "bg-secondaryGray-900"
+                              : "bg-secondaryGray-300"
+                          } px-2 py-2 rounded w-[80px] h-[80px] flex flex-col justify-center items-center`}
                         >
-                          <FiLink className="text-3xl text-gray-700" />
-                          <p>{ref.desc}</p>
-                        </a>
-                      </div>
-                    </div>
-                    <div className="w-full text-center">
-                      <p>{ref.name}</p>
-                    </div>
-                  </div>
+                          <FiLink className="text-3xl" />
+                          <Text>{ref.desc}</Text>
+                        </Link>
+                      </Box>
+                    </Box>
+                    <Box className="w-full text-center">
+                      <Text>{ref.name}</Text>
+                    </Box>
+                  </Box>
                 );
               })}
-            </div>
-          </div>
+            </Box>
+          </Box>
 
-          <div className="w-full flex flex-col gap-5">
-            <div className="w-full md:text-3xl text-xl font-semibold text-gray-700 border-b-2 border-red-500">
-              <h1>VISI MISI DAN TUJUAN</h1>
-            </div>
-            <div className="flex md:flex-row flex-col md:gap-10 gap-5">
+          <Flex className="w-full flex flex-col gap-5 ">
+            <Box
+              className={`w-full md:text-3xl text-xl font-semibol border-b-2 ${
+                colorMode === "dark" ? "border-yellow-600" : "border-red-600"
+              }`}
+            >
+              <Heading at="h1">VISI MISI DAN TUJUAN</Heading>
+            </Box>
+            <Box className="flex md:flex-row flex-col md:gap-10 gap-5">
               {/* Card Visi */}
-              <div className="w-full flex flex-col md:gap-10 gap-5">
-                <div className="card w-full h-fit bg-base-100 shadow">
-                  <figure className="w-full h-[150px] border-b-2 border-red-500 ">
-                    <img
+              <Flex className="w-full flex flex-col md:gap-10 gap-5">
+                <Card
+                  className={`card w-full h-fit bg-base-100 shadow ${
+                    colorMode === "dark"
+                      ? "bg-secondaryGray-900"
+                      : "bg-secondaryGray-300"
+                  }`}
+                >
+                  <figure
+                    className={`w-full h-[150px] border-b-2  ${
+                      colorMode === "dark"
+                        ? "border-yellow-600"
+                        : "border-red-600"
+                    }`}
+                  >
+                    <Image
                       className="w-full h-full object-contain"
                       src={logoPolitama}
                       alt="Shoes"
                     />
                   </figure>
-                  <div className="card-body">
-                    <h2 className="card-title">VISI</h2>
+                  <Box className="card-body">
+                    <Text as="h2" className="card-title">
+                      VISI
+                    </Text>
                     {visi.map((item, i) => (
-                      <div key={i} className="flex gap-2 w-full h-fit">
-                        <span className="text-gray-800 font-bold">
-                          {i + 1}.
-                        </span>
-                        <p>{item.text}</p>
-                      </div>
+                      <Flex key={i} className="flex gap-2 w-full h-fit">
+                        <Text className="font-bold">{i + 1}.</Text>
+                        <Text>{item.text}</Text>
+                      </Flex>
                     ))}
-                  </div>
-                </div>
-                <div className="md:block  hidden card w-full h-fit bg-base-100 shadow">
-                  <div className="card-body">
-                    <h2 className="card-title">TUJUAN</h2>
+                  </Box>
+                </Card>
+                <Box className="md:block  hidden card w-full h-fit bg-base-100 shadow">
+                  <Card
+                    className={`card-body ${
+                      colorMode === "dark"
+                        ? "bg-secondaryGray-900"
+                        : "bg-secondaryGray-300"
+                    }`}
+                  >
+                    <Heading as="h2" className="card-title">
+                      TUJUAN
+                    </Heading>
                     {tujuan.map((item, i) => (
-                      <div key={i} className="flex gap-2 w-full h-fit">
-                        <span className="text-gray-800 font-bold">
-                          {i + 1}.
-                        </span>
-                        <p>{item.text}</p>
-                      </div>
+                      <Flex key={i} className="flex gap-2 w-full h-fit">
+                        <Text className="font-bold">{i + 1}.</Text>
+                        <Text>{item.text}</Text>
+                      </Flex>
                     ))}
-                  </div>
-                </div>
-              </div>
+                  </Card>
+                </Box>
+              </Flex>
               {/* Card Misi */}
-              <div className="w-full flex flex-col md:gap-10 gap-5">
-                <div className="card w-full h-fit bg-base-100 shadow">
-                  <figure className="w-full h-[150px] border-b-2 border-red-500 ">
-                    <img
+              <Flex className="w-full flex flex-col md:gap-10 gap-5">
+                <Card
+                  className={`card w-full h-fit bg-base-100 shadow ${
+                    colorMode === "dark"
+                      ? "bg-secondaryGray-900"
+                      : "bg-secondaryGray-300"
+                  }`}
+                >
+                  <figure
+                    className={`w-full h-[150px] border-b-2  ${
+                      colorMode === "dark"
+                        ? "border-yellow-600"
+                        : "border-red-600"
+                    }`}
+                  >
+                    <Image
                       className="w-full h-full object-contain"
                       src={logoPolitama}
                       alt="Shoes"
                     />
                   </figure>
-                  <div className="card-body">
-                    <h2 className="card-title">MISI</h2>
+                  <Card
+                    className={`card-body rounded-t-none ${
+                      colorMode === "dark"
+                        ? "bg-secondaryGray-900"
+                        : "bg-secondaryGray-300"
+                    }`}
+                  >
+                    <Heading as="h2" className="card-title">
+                      MISI
+                    </Heading>
                     {mission.map((misi, i) => (
-                      <div key={i} className="flex gap-3">
-                        <p className="text-gray-800 font-bold">{i + 1}.</p>
-                        <h6>{misi.parent}</h6>
-                      </div>
+                      <Flex key={i} className="flex gap-3">
+                        <Text className="font-bold">{i + 1}.</Text>
+                        <Text>{misi.parent}</Text>
+                      </Flex>
                     ))}
-                  </div>
-                </div>
-                <div className="md:hidden card w-full h-fit bg-base-100 shadow">
-                  <div className="card-body">
-                    <h2 className="card-title">TUJUAN</h2>
+                  </Card>
+                </Card>
+                <Card
+                  className={`md:hidden card w-full h-fit bg-base-100 shadow ${
+                    colorMode === "dark"
+                      ? "bg-secondaryGray-900"
+                      : "bg-secondaryGray-300"
+                  }`}
+                >
+                  <Box className="card-body">
+                    <Heading as="h2" className="card-title">
+                      TUJUAN
+                    </Heading>
                     {tujuan.map((item, i) => (
-                      <div key={i} className="flex gap-2 w-full h-fit">
-                        <span className="text-gray-800 font-bold">
-                          {i + 1}.
-                        </span>
-                        <p>{item.text}</p>
-                      </div>
+                      <Flex key={i} className="flex gap-2 w-full h-fit">
+                        <Text className="font-bold">{i + 1}.</Text>
+                        <Text>{item.text}</Text>
+                      </Flex>
                     ))}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+                  </Box>
+                </Card>
+              </Flex>
+            </Box>
+          </Flex>
+        </Box>
       </>
     </Layout>
   );
