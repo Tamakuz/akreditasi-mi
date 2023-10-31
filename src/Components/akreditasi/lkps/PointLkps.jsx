d7510a255d93f61f7c7194e65be99920c5258ce9
mancode <mancode700@gmail.com>
2023-10-31 12:00:00 +0700

    Update: Pendukung AKreditasiLKPS

--- src/Components/akreditasi/lkps/PointLkps.jsx
+++ src/Components/akreditasi/lkps/PointLkps.jsx
@@ -9,48 +9,12 @@ import {
   useColorMode,
 } from "@chakra-ui/react";
 import TamplateAkreditasi from "./pointLkps/template";
+import DTamplate from "./../../../Components/akreditasi/led/bab2/Component/dTamplate"
 
 const PointLkps = () => {
   const { colorMode } = useColorMode();
   return (
-    <Accordion allowMultiple>
-      <AccordionItem
-        className={`${
-          colorMode === "dark" ? "bg-secondaryGray-900" : "bg-secondaryGray-300"
-        } border-none rounded-xl`}
-      >
-        <Text>
-          <AccordionButton className="text-xl py-3">
-            <Box as="span" flex="1" textAlign="left">
-              Point Lkps
-            </Box>
-            <AccordionIcon />
-          </AccordionButton>
-        </Text>
-        <AccordionPanel pb={3} className="flex flex-col gap-2">
-          <TamplateAkreditasi
-            endpoint={"a"}
-            title={"Tata Pamong, Tata Kelola, dan Kerjasama"}
-          />
-          <TamplateAkreditasi
-            endpoint={"a"}
-            title={"Visi, Misi, Tujuan dan Strategi"}
-          />
-          <TamplateAkreditasi
-            endpoint={"a"}
-            title={"Mahasiswa"}
-          />
-          <TamplateAkreditasi
-            endpoint={"a"}
-            title={"Sumber Daya Manusia"}
-          />
-          <TamplateAkreditasi
-            endpoint={"a"}
-            title={"Keuangan, Sarana dan Prasarana"}
-          />
-        </AccordionPanel>
-      </AccordionItem>
-    </Accordion>
+    <DTamplate endpoint={"poin-lkps"} title={"Point LKPS"} />
   );
 };
 
